e2afb6875370905d59cb36ccc3af46cbf018df8e
Merlin Mason <info@merlinmason.co.uk>
2018-01-24 23:28:05 +0000

    Create fish item template

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { formatPrice } from "../helpers";
 
 class Fish extends React.Component {
     render() {
+        // extract details from this.props with es6 destructuring (saves typing this.props a lot in the template)
+        const { details } = this.props;
         return(
-            <li className="menu-fish">fish</li>
+            <li className="menu-fish">
+                <img src={details.image} alt={details.name}/>
+                <h3 className="fish-name">
+                    {details.name}
+                    <span className="price">{formatPrice(details.price)}</span>
+                </h3>
+                <p>{details.desc}</p>
+                <button>Add To Order</button>
+            </li>
         )
     }
 }
