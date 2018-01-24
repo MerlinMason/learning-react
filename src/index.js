0aba39c9c5403fcde143390a5d2b681464e1f73c
Merlin Mason <info@merlinmason.co.uk>
2018-01-24 23:04:25 +0000

    Create router and route between store picker and main app

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { render } from "react-dom";
+import { BrowserRouter, Match, Miss } from "react-router";
+
+import "./css/style.css";
+import StorePicker from "./components/StorePicker";
+import App from "./components/App";
+import NotFound from "./components/NotFound";
+
+const Root = () => {
+    return (
+        <BrowserRouter>
+            <div>
+                <Match exactly pattern="/" component={StorePicker} />
+                <Match exactly pattern="/store/:storeId" component={App} />
+                <Miss component={NotFound} />
+            </div>
+        </BrowserRouter>
+    )
+}
+render(<Root/>, document.querySelector("#main"));
+
